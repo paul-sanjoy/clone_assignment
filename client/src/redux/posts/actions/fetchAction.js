66606a7a7fcb3c90268b5c6e91cbe13493391c88
paul-sanjoy <sanjoyworkspace@gmail.com>
2022-01-29 02:16:24 +0530

    Update fetchAction.js

--- client/src/redux/posts/actions/fetchAction.js
+++ client/src/redux/posts/actions/fetchAction.js
@@ -5,11 +5,9 @@ export function fetchPosts() {
         dispatch({ type: "POSTS_FETCH_REQUEST", payload: {} })
         axios.get("https://fswi-99acres-clone.herokuapp.com/cities")
             .then(response => {
-                // pass data to reducer in case of success
                 dispatch({ type: "POSTS_FETCH_SUCCESS", payload: response.data })
             })
             .catch(error => {
-                //we have to tell reducer to show error message
                 dispatch({ type: "POSTS_FETCH_FAILURE", payload: "Something went wrong" })
             })
 
